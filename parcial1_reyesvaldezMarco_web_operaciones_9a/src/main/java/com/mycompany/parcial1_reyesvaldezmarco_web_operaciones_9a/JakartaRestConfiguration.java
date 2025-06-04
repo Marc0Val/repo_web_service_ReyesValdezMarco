1d009a606a08557e9b7c90e81d42efc9eae6245a
Marco Valdez <128828459+Marc0Val@users.noreply.github.com>
2025-06-04 15:30:43 -0600

    innitialCommit

---
+++ parcial1_reyesvaldezMarco_web_operaciones_9a/src/main/java/com/mycompany/parcial1_reyesvaldezmarco_web_operaciones_9a/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.parcial1_reyesvaldezmarco_web_operaciones_9a;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ parcial1_reyesvaldezMarco_web_operaciones_9a/src/main/java/com/mycompany/parcial1_reyesvaldezmarco_web_operaciones_9a/calcularIntensidad.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package com.mycompany.parcial1_reyesvaldezmarco_web_operaciones_9a;
+
+import jakarta.jws.WebService;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+
+/**
+ *
+ * @author Dispositivo
+ */
+@WebService(serviceName = "calcularIntensidad")
+public class calcularIntensidad {
+
+    /**
+     * This is a sample web service operation
+     *
+     * @param voltaje
+     * @param resistencia
+     * @return
+     */
+    @WebMethod(operationName = "calcularIntensidad")
+    public double calcularIntensidad(@WebParam(name = "voltaje") double voltaje, @WebParam(name = "resistencia") double resistencia) {
+        if (resistencia == 0) {
+            throw new IllegalArgumentException("no puede se cero 0");
+        }
+        return voltaje / resistencia;
+    }
+
+}
